78057a295a50289c31f6264339f6d0833f2d072f
Per Wagner Nielsen <perwagnernielsen@gmail.com>
2022-04-14 12:43:47 +0200

    Fix tests

--- company/tests.py
+++ company/tests.py
@@ -6,9 +6,7 @@ def test_get_the_api_response():
     url = "http://localhost:8080/helloworld"
     r = requests.get(url)
 
-    print(r)
-    print(r.text)
     assert r.status_code == 200
-
+    assert r.text == "hello world"
 
 
